21359e6ff45657d9fd79af63b74f77c8b427d1ef
Roy Kolak <roy.kolak@gmail.com>
2016-08-17 22:12:12 -0500

    rework programmable attachment

--- displays/programmable-display.js
+++ displays/programmable-display.js
@@ -5,36 +5,33 @@ var firebase = require("firebase");
 class ProgrammableDisplay {
   constructor(modeData, dimensions, callbacks) {
     this.matrixKey = modeData.matrix;
-    this.matrixRef = firebase.database().ref(`matrices/${this.matrixKey}`);
     this.callbacks = callbacks;
   }
 
-  onceValueListner(snapshot) {
-    var data = snapshot.val();
+  start() {
+    var matrixRef = firebase.database().ref(`matrices/${this.matrixKey}`);
+    
+    matrixRef.once('value').then((snapshot) => {
+      var data = snapshot.val();
 
-    for(let key in snapshot.val()) {
-      var hex = data[key].hex,
-          [y, x] = key.split(':');
+      for(let key in snapshot.val()) {
+        var hex = data[key].hex,
+            [y, x] = key.split(':');
+
+        this.callbacks.onPixelChange(y, x, hex);
+      }
+    });
+
+    this.childChangedRef = this.matrixRef.on('child_changed', (snapshot) => {
+      var hex = snapshot.val().hex,
+          [y, x] = snapshot.key.split(':');
 
       this.callbacks.onPixelChange(y, x, hex);
-    }
-  }
-
-  childChangedListner(snapshot) {
-    var hex = snapshot.val().hex,
-        [y, x] = snapshot.key.split(':');
-
-    this.callbacks.onPixelChange(y, x, hex);
-  }
-
-  start() {
-    this.matrixRef.once('value').then(this.onceValueListner);
-    this.matrixRef.on('child_changed', this.childChangedListner);
+    });
   }
 
   stop() {
-    this.matrixRef.off(this.onceValueListner);
-    this.matrixRef.off(this.childChangedListner);
+    this.childChangedRef.off();
   }
 }
 
