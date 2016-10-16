e8699a6f89e0abc52114f9ca09fe6ef5c2ed1bf6
Roy Kolak <roy.kolak@gmail.com>
2016-10-16 10:13:21 -0500

    simplify and integrate macros repo

--- index.js
+++ index.js
@@ -1,100 +1,48 @@
 "use strict";
 
-var MacroLibrary = require('macro-library');
-
-var macroLibrary = new MacroLibrary();
-macroLibrary.registerMacros();
+var Macros = {
+  'programmable': require('macros/lib/programmable'),
+  'twinkle': require('macros/lib/twinkle'),
+  'solid-color': require('macros/lib/solid-color'),
+  'unsupported': require('macros/lib/unsupported'),
+  'marquee': require('macros/lib/marquee')
+};
 
 class DisplayCoupler {
   constructor(db) {
     this.db = db;
-    this.startingUp = true;
-  }
-
-  static registeredMacros() {
-    return macroLibrary.registeredMacros();
-  }
-
-  startUp({dimensions, callbacks}) {
-    this.activateMacro = macroLibrary.loadMacro('start-up', {
-      dimensions: dimensions,
-      callbacks: callbacks
-    });
-    this.activateMacro.start();
-  }
-
-  demo(displayConfig, callbacks) {
-    var next = () => {
-      var macro = displayConfig.macro,
-          options = {
-            config: displayConfig.macroConfig || {},
-            dimensions: {
-              width: displayConfig.width,
-              height: displayConfig.height
-            },
-            callbacks: {
-              onPixelChange: (y, x, hex) => {
-                callbacks.onPixelChange(y, x, hex, displayConfig);
-              }
-            }
-          };
-
-      if(this.activateMacro) {
-        this.activateMacro.stop();
-      }
-      this.activateMacro = macroLibrary.loadMacro(macro, options);
-      this.activateMacro.start();
-    };
-
-    if(this.startingUp) {
-      callbacks.onReady(displayConfig, () => {
-        this.startingUp = false;
-        next();
-      });
-    } else {
-      next()
-    }
   }
 
   connect(displayKey, callbacks) {
     this.db.ref(`displays/${displayKey}/`).on('value', (snapshot) => {
-      var displayData = snapshot.val();
-
-      var next = () => {
-        var macro = displayData.macro,
-            options = {
-              config: displayData.macroConfig || {},
-              dimensions: {
-                width: displayData.width,
-                height: displayData.height
-              },
-              db: this.db,
-              callbacks: {
-                onPixelChange: (y, x, hex) => {
-                  callbacks.onPixelChange(y, x, hex, displayData);
-                }
+      var displayData = snapshot.val(),
+          options = {
+            config: displayData.macroConfig || {},
+            dimensions: {
+              width: displayData.width,
+              height: displayData.height
+            },
+            db: this.db,
+            callbacks: {
+              onPixelChange: (y, x, hex) => {
+                callbacks.onPixelChange(y, x, hex, displayData);
               }
-            };
+            }
+          };
 
-        if(macro === "programmable") {
-          options.config.matrix = displayData.matrix;
-        }
-
-        if(this.activateMacro) {
-          this.activateMacro.stop();
-        }
-        this.activateMacro = macroLibrary.loadMacro(macro, options);
-        this.activateMacro.start();
-      };
-
-      if(this.startingUp) {
-        callbacks.onReady(displayData, () => {
-          this.startingUp = false;
-          next();
-        });
-      } else {
-        next()
+      if(displayData.macro === "programmable") {
+        options.config.matrix = displayData.matrix;
       }
+
+      if(this.activateMacro) {
+        this.activateMacro.stop();
+      }
+
+      var Macro = Marcos[displayData.macro] || Macros.unsupported;
+
+      this.activateMacro = new Marco(options);
+
+      this.activateMacro.start();
     });
   }
 }
