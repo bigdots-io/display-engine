27bf5289121d62f828f495a8677af5cf4ade3010
Roy Kolak <roy.kolak@gmail.com>
2016-10-27 23:32:47 -0500

    implement offline support

--- index.js
+++ index.js
@@ -12,64 +12,60 @@ var Macros = {
 };
 
 class DisplayCoupler {
-  constructor(db) {
+  constructor(db, dimensions) {
     this.db = db;
+    this.dimensions = dimensions;
   }
 
   connect(displayKey, callbacks) {
     this.db.ref('.info/connected').on('value', (snapshot) => {
       if(!snapshot.val()) {
-        if(this.activateMacro) {
-          this.activateMacro.stop();
-        }
+        this.renderAsOffline(callbacks.onPixelChange);
+      } else {
+        this.db.ref(`displays/${displayKey}/`).on('value', (snapshot) => {
+          var displayData = snapshot.val();
 
-        this.activateMacro = new Macros['offline']({
-          config: {},
-          dimensions: {
-            width: 128,
-            height: 16
-          },
-          db: this.db,
-          callbacks: {
-            onPixelChange: (y, x, hex) => {
-              callbacks.onPixelChange(y, x, hex);
-            }
-          }
-        });
-        this.activateMacro.start();
-      }
-    });
-
-    this.db.ref(`displays/${displayKey}/`).on('value', (snapshot) => {
-      var displayData = snapshot.val(),
-          options = {
+          var options = {
             config: displayData.macroConfig || {},
-            dimensions: {
-              width: displayData.width,
-              height: displayData.height
-            },
+            dimensions: this.dimensions,
             db: this.db,
             callbacks: {
-              onPixelChange: (y, x, hex) => {
-                callbacks.onPixelChange(y, x, hex, displayData);
-              }
+              onPixelChange: callbacks.onPixelChange
             }
           };
 
-      if(displayData.macro === "programmable") {
-        options.config.matrix = displayData.matrix;
+          if(displayData.macro === "programmable") {
+            options.config.matrix = displayData.matrix;
+          }
+
+          if(this.activateMacro) {
+            this.activateMacro.stop();
+          }
+
+          var Macro = Macros[displayData.macro] || Macros.unsupported;
+
+          this.activateMacro = new Macro(options);
+
+          this.activateMacro.start();
+        });
       }
-
-      if(this.activateMacro) {
-        this.activateMacro.stop();
-      }
-
-      var Macro = Macros[displayData.macro] || Macros.unsupported;
-
-      this.activateMacro = new Macro(options);
-
-      this.activateMacro.start();
     });
+  }
+
+  renderAsOffline(onPixelChange) {
+    if(this.activateMacro) {
+      this.activateMacro.stop();
+    }
+
+    this.activateMacro = new Macros['offline']({
+      config: {},
+      dimensions: this.dimensions,
+      db: this.db,
+      callbacks: {
+        onPixelChange: onPixelChange
+      }
+    });
+    this.activateMacro.start();
   }
 }
 
