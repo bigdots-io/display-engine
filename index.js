3132aa64aed339b2371a0ab78dbd2340f774afe1
Roy Kolak <roy.kolak@gmail.com>
2016-12-11 12:09:52 -0600

    disable connection status checker

--- index.js
+++ index.js
@@ -26,29 +26,30 @@ class DisplayCoupler {
   }
 
   connect(displayKey, callbacks) {
+    // DISABLE CONNECTION CHECKER FOR NOW.... super buggy!
     // Use a connection listener as the master loop so that the active macro
     // can be overriden by an "offline macro".
-    this.db.ref('.info/connected').on('value', (snapshot) => {
-      if(!snapshot.val()) {
-        var delay = 0;
-
-        // Prevent false positives because firebase thinks it's not connected for
-        // a split second when it starts up for the first time.
-        if(this.connectionStatus == status.unknown) {
-          delay = 2000;
-        } else {
-          this.connectionStatus = status.notConnected
-        }
-
-        setTimeout(() => {
-          if(this.connectionStatus !== status.connected) {
-            this.renderAsOffline(callbacks.onPixelChange);
-            this.connectionStatus = status.notConnected;
-          }
-        }, delay);
-
-      } else {
-        this.connectionStatus = status.connected;
+    // this.db.ref('.info/connected').on('value', (snapshot) => {
+      // if(!snapshot.val()) {
+      //   var delay = 0;
+      //
+      //   // Prevent false positives because firebase thinks it's not connected for
+      //   // a split second when it starts up for the first time.
+      //   if(this.connectionStatus == status.unknown) {
+      //     delay = 2000;
+      //   } else {
+      //     this.connectionStatus = status.notConnected
+      //   }
+      //
+      //   setTimeout(() => {
+      //     if(this.connectionStatus !== status.connected) {
+      //       this.renderAsOffline(callbacks.onPixelChange);
+      //       this.connectionStatus = status.notConnected;
+      //     }
+      //   }, delay);
+      //
+      // } else {
+      //   this.connectionStatus = status.connected;
         this.db.ref(`displays/${displayKey}/`).on('value', (snapshot) => {
           var displayData = snapshot.val();
 
@@ -77,8 +78,8 @@ class DisplayCoupler {
 
           this.activateMacro.start();
         });
-      }
-    });
+    //   }
+    // });
   }
 
   renderAsOffline(onPixelChange) {
