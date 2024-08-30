521f930da6186f9693e1e8d669a2e4f4a749e996
Roy Kolak <roykolak@fastmail.com>
2024-08-29 21:59:36 -0500

    fix meteor macro

--- src/macros/meteors.ts
+++ src/macros/meteors.ts
@@ -5,8 +5,7 @@ import { hexToRgb } from "./ripple.js";
 interface Meteor {
   tailLength: number;
   speed: number;
-  //   colors: string[];
-  //   rgb: Uint8ClampedArray;
+  depth: number;
   moveCount: number;
   complete: boolean;
   startingX: number;
@@ -21,11 +20,11 @@ export const startMeteors: MacroFn = async ({
   updatePixels,
 }) => {
   const config = {
-    color: "#FFF",
+    color: "#FFFFFF",
     meteorCount: 40,
     maxTailLength: 20,
     minTailLength: 5,
-    maxDepth: 5,
+    maxDepth: 10,
     minSpeed: 100,
     maxSpeed: 10,
     width: dimensions.width,
@@ -34,6 +33,8 @@ export const startMeteors: MacroFn = async ({
     ...macroConfig,
   };
 
+  const rgb = hexToRgb(config.color);
+
   const meteors: Meteor[] = [];
   const validStartingPoints: number[] = [];
 
@@ -41,15 +42,13 @@ export const startMeteors: MacroFn = async ({
     validStartingPoints.push(i);
   }
 
-  const rgb = hexToRgb(config.color);
-
   const generateMeteor = (): Meteor => {
     const tailLength =
       Math.floor(
         Math.random() * (config.maxTailLength - config.minTailLength)
       ) + config.minTailLength;
 
-    // const depth = Math.floor(Math.random() * (config.maxDepth - 1)) + 1;
+    const depth = Math.floor(Math.random() * config.maxDepth) + 1;
 
     const startingX =
       validStartingPoints[
@@ -61,7 +60,7 @@ export const startMeteors: MacroFn = async ({
       speed:
         Math.floor(Math.random() * (config.minSpeed - config.maxSpeed)) +
         config.maxSpeed,
-      //   colors: generateColorShade(config.color, tailLength, depth),
+      depth,
       moveCount: 0,
       complete: false,
       startingX: startingX,
@@ -79,9 +78,6 @@ export const startMeteors: MacroFn = async ({
     meteors.push(meteor);
     const index = validStartingPoints.indexOf(meteor.path[0].x);
     validStartingPoints.splice(index, 1);
-
-    // ctx.fillStyle = meteor.colors[0];
-    // ctx.fillRect(meteor.path[0].y, meteor.path[0].x, 1, 1);
   };
 
   for (let i = 0; i < config.meteorCount; i++) {
@@ -116,18 +112,17 @@ export const startMeteors: MacroFn = async ({
           validStartingPoints.push(meteor.startingX);
         }
 
+        const meteorDepthAlphaPercentage = meteor.depth / config.maxDepth;
+
         meteor.path.forEach((dot, i) => {
-          ctx.fillStyle = "#fff";
-          ctx.fillRect(dot.y, dot.x, 1, 1);
-
-          const rgb = hexToRgb(config.color);
-
+          const meteorDotAlphaPercentage =
+            (meteor.tailLength - i) / meteor.tailLength;
           const id = ctx.createImageData(1, 1);
           const d = id.data;
           d[0] = rgb?.r as number;
           d[1] = rgb?.g as number;
           d[2] = rgb?.b as number;
-          //   d[3] = (adjustedHeight / 100) * 255;
+          d[3] = meteorDotAlphaPercentage * meteorDepthAlphaPercentage * 255;
           ctx.putImageData(id, dot.x, dot.y);
         });
       }
@@ -139,22 +134,3 @@ export const startMeteors: MacroFn = async ({
 
   return Promise.resolve(() => clearInterval(interval));
 };
-
-// function generateColorShade(seedColor: string, length: number, depth: number) {
-//   const colors = [],
-//     interval = 1 / (length - 1);
-
-//   if (depth !== 1) {
-//     seedColor = colorLuminance(seedColor, Math.round(-(1 / depth) * 10) / 10);
-//   }
-
-//   for (let i = 0; i < 1; i = i + interval) {
-//     colors.push(colorLuminance(seedColor, -i));
-//   }
-
-//   if (colors.length < length) {
-//     colors.push("#000000");
-//   }
-
-//   return colors;
-// }
